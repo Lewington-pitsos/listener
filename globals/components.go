4ce76b8db538438ed0d4f77b7cd34274502dd4b4
Louka Ewington-Pitsos <lewington@student.unimelb.edu.au>
2019-08-03 22:15:22 +1000

    add assets and global variables

---
+++ globals/components.go
@@ -0,0 +1,10 @@
+package globals
+
+type Component string
+
+const (
+	ComponentRouter  Component = "router"
+	ComponentBettor  Component = "bettor"
+	ComponentScraper Component = "scraper"
+	ComponentBalance Component = "balance"
+)
---
+++ globals/globals.go
@@ -0,0 +1,7 @@
+package globals
+
+const AzureIP = "40.115.67.85"
+
+var HubPort = ":8083"
+
+const HubIP = AzureIP
